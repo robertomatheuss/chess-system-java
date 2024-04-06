bd02ae4dd08a57c21a037addb8cfe3dbb0f2f9bd
Roberto Matheus <robertmatheusa@gmail.com>
2024-04-06 13:51:47 -0300

    Created class: Position

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,7 +1,11 @@
 package org.example.application;
 
+import org.example.boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(2,2);
+        System.out.println(position);
+
     }
 }
